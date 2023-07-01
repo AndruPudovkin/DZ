52efd80773db21229a3a5068f014e159d4979e6b
Andru <pudowkin98@mail.ru>
2023-07-02 01:58:34 +0500

    Task_6

---
+++ Task_6/Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int[] FillArray(int size)
+{
+    int[] arrayfill = new int[size];
+    for (int i =0; i < arrayfill.Length; i++)
+    {
+        arrayfill[i] = Convert.ToInt32(System.Console.ReadLine());
+    }
+return arrayfill;
+}
+//
+void PrinArray(int[] arrayPrint)
+{
+    System.Console.WriteLine("[" + string.Join(",  ", arrayPrint) + "]");
+}
+//
+int Function(int[] arrayfanction)
+{
+    int count = 0;
+    for (int i =0; i < arrayfanction.Length; i++)
+    {
+        if (arrayfanction[i]>0)
+        count++;
+    }
+return count;
+}
+//
+void Main()
+{
+    System.Console.WriteLine("Введите число М: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] array = FillArray(m);
+    PrinArray(array);
+    int count = Function(array);
+    System.Console.WriteLine(count);
+}
+//-------------------------------------------------
+Main();
---
+++ Task_6/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int Variables (string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//
+void Function(int k1, int b1, int k2, int b2, out int x, out int y)
+{
+    x = 0;
+    y = 0;
+    x = (b2-b1)/(k1-k2);
+    y = k1*x+b1;
+}
+void Main()
+{
+    int k1 = Variables("Введите К1: ");
+    int b1 = Variables("Введите B1: ");
+    int k2 = Variables("Введите К2: ");
+    int b2 = Variables("Введите B2: ");
+    Function(k1, b1, k2, b2, out int x, out int y);
+    System.Console.WriteLine($"Точка пересечения прямых имеет коардинаты ({x},{y})");
+}
+//----------------------------------------------------
+Main();
