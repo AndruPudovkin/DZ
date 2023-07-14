1952feaf96928d2b08d072eca5add09a6972dd9b
Andru <pudowkin98@mail.ru>
2023-07-14 20:01:54 +0500

    Task_9

---
+++ Task_9/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Sum(int numM, int numN)
+{
+    if(numM>numN) return 0;
+    return numM+Sum(numM+1,numN);
+}
+//---------------------------
+int m = ReadInt("Введите петвое число ");
+int n = ReadInt("Введите второе число ");
+if (n>m) 
+{
+    System.Console.WriteLine(Sum(m,n));
+}
+else
+{
+    System.Console.WriteLine(Sum(n,m));
+}
---
+++ Task_9/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A(int numM, int numN)
+{
+    if(numM==0) return numN+1;
+    else if(numM > 0 && numN==0) return A(numM-1,1);
+    else  return A(numM-1,A(numM,numN-1));
+}
+//----------------
+int m = ReadInt("Введите петвое число ");
+int n = ReadInt("Введите второе число ");
+System.Console.WriteLine(A(m,n));
